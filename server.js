dc2066349aa8f5ed0a77dba667e6dbb555ac6229
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-17 22:18:00 +0100

    Initialize project

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.engine('.hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+app.get('/error.jpeg', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/error.jpeg'));
+  });
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res, next) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
